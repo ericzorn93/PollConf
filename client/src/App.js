529c3cfe6f91bc554c307566b3b2b6b250351935
Eric Zorn <zornwebdev@gmail.com>
2018-10-27 14:19:18 -0400

    Add Home Component and React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.scss";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Home Page</h1>
-      </div>
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+      </Switch>
     );
   }
 }
