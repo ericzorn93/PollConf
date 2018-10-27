50cf825b0ec6c2781973e0ed2476c321e3002d81
Eric Zorn <zornwebdev@gmail.com>
2018-10-27 14:21:25 -0400

    Add Cors module to server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
 const chalk = require("chalk");
+const cors = require("cors");
 
 // Routing Imports
 const pollRoutes = require("./routes/pollRoutes");
@@ -16,6 +17,7 @@ const app = express();
 // Serverside middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // Port Setup
 const port = process.env.PORT || 3000;
