e24971b6fb7561ead2423cca64b8109e3cbdaea8
Eric Zorn <zornwebdev@gmail.com>
2018-10-27 13:55:15 -0400

    Added database setup

--- routes/answerRoutes.js
+++ routes/answerRoutes.js
@@ -3,6 +3,10 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+// Middlware and setup
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+
 // Get routes
 router.get("/all-answers", (req, res) => {
   res.json({
