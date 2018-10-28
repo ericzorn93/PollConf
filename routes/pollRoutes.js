455ba7bbf673fdc7e28522fdb1d75ef74e407f0c
Eric Zorn <zornwebdev@gmail.com>
2018-10-28 19:43:03 -0400

    Added Poll Schema for Mongoose

--- routes/pollRoutes.js
+++ routes/pollRoutes.js
@@ -2,10 +2,21 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 // Middlware and setup
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
+
+// Database Setup
+const Schema = mongoose.Schema;
+
+const pollSchema = new Schema({
+  title: String,
+  author: String,
+  question: String,
+  created_at: moment(new Date()).format("MMMM Do YYYY, h:mm:ss a")
+});
 
 // Get routes
 router.get("/all-polls", (req, res) => {
