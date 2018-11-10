aedc3a2aaaf9831e382d7da27ba3985c802ed1c6
Eric Zorn <zornwebdev@gmail.com>
2018-11-10 12:28:13 -0500

    Changed Server Port to 3001 and Updated Proxy on Client

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Port Setup
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Routing
 app.use("/api/poll", pollRoutes);
